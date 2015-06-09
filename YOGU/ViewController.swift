0c150357bee2cd3e09de92f14af9b24575c0b6a6
志迪 夏 <zhidi.xia@dianping.com>
2015-06-09 19:22:03 +0800

    创建项目

---
+++ YOGU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YOGU
+//
+//  Created by Zhidi Xia on 15/6/9.
+//  Copyright (c) 2015年 chentoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
